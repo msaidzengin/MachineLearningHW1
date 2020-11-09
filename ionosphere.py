047999224765c63607815957bb5c46d0544f4d58
msaidzengin <37325455+msaidzengin@users.noreply.github.com>
2020-11-09 11:48:20 +0300

    results splitted as 1 and 0

--- ionosphere.py
+++ ionosphere.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-data = np.genfromtxt('ionosphere/ionosphere.data', delimiter=',', dtype=None, encoding='utf-8')
+data = np.genfromtxt('dataset/ionosphere/ionosphere.data', delimiter=',', dtype=None, encoding='utf-8', converters={34: lambda x: 1 if x == 'g' else 0})
 np.random.shuffle(data)
 trainLen = int(len(data) * 0.8)
 train, test = data[:trainLen], data[trainLen:]
-
-print(len(data))
-print(len(train))
-print(len(test))
-
+trainResult = [i[-1] for i in train]
+train = [list(t)[:-1] for t in train]
+testResult = [i[-1] for i in test]
+test = [list(t)[:-1] for t in test]
