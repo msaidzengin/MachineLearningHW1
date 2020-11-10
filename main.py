2e8be8540f690cd65c92dadc8c50f4f585dceebf
msaidzengin <37325455+msaidzengin@users.noreply.github.com>
2020-11-10 11:01:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ import numpy as np
 def main():
     process('dataset/ionosphere/ionosphere.data', 35, 'g', 'b', 8)
     process('dataset/connectionistBench/sonar.all-data', 61, 'R', 'M', 16)
-    
 
 def process(filename, col, a, b, batch):
 
     train, trainResult, test, testResult = prepare(filename, col, a, b)
 
-    testAcc = []
-    trainAcc = []
-    iteration = range(200)
+    testAcc, trainAcc = [], []
+    iteration = range(100)
     for i in iteration:
         betas = np.zeros((train.shape[1],1))
         betas, cost_history = iterateGradientDescent(train, trainResult, betas, i, batch)
@@ -20,14 +18,13 @@ def process(filename, col, a, b, batch):
         trainAcc.append(calculateAccuracy(betas, train, trainResult))
 
     plt.figure(figsize=(20,10))
-    plt.suptitle("Dataset: "+filename)
+    plt.suptitle(filename)
     plt.subplot(2, 1, 1)
     plt.title('Cost-Iteration')
-    plt.plot(cost_history, label='cost-'+str(i))
+    plt.plot(cost_history, label='cost')
     plt.subplot(2, 1, 2)
     plt.title('Accuracy-Iteration')
-    plt.plot(iteration, testAcc, label='Test Data')
-    plt.plot(iteration, trainAcc, label='Training Data')
+    plt.plot(iteration, trainAcc, label='acc')
     plt.legend(loc='lower right')
     plt.show()
 
@@ -50,17 +47,13 @@ def prepare(filename, col, a, b):
 
     return train, trainResult, test, testResult
 
-
 def splitBatches(data, dataResults, batchSize):
-    splitData=[]
-    splitDataResults=[]
-    batchCount=data.shape[0] // batchSize
-    for i in range(batchCount):
-        splitData.append(data[(i) * batchSize : (i+1) * batchSize, :])
-        splitDataResults.append(dataResults[(i) * batchSize : (i+1) * batchSize, :])
-    splitData=np.asarray(splitData)
-    splitDataResults=np.asarray(splitDataResults)
-    return splitData, splitDataResults, batchCount
+    split, results = [], []
+    count = data.shape[0] // batchSize
+    for i in range(count):
+        split.append(data[(i) * batchSize : (i+1) * batchSize, :])
+        results.append(dataResults[(i) * batchSize : (i+1) * batchSize, :])
+    return np.asarray(split), np.asarray(results), count
 
 def sigmoid(x):
     return 1.0/(1.0 + np.exp(-1.0 * x))
@@ -69,30 +62,27 @@ def hypothesis(data, betas):
     return sigmoid(np.dot(data,betas))
 
 def calculateCost(data, dataResults, betas):
-    m=data.shape[0]
-    predicts=hypothesis(data, betas)
-    cost=dataResults*np.log(predicts) + (1-dataResults)*np.log(1-predicts)
-    cost=cost.sum()/(-1*m)
+    m = data.shape[0]
+    predicts = hypothesis(data, betas)
+    cost = dataResults * np.log(predicts) + (1-dataResults) * np.log(1-predicts)
+    cost = cost.sum() / (-1*m)
     return cost
 
 def updateBetas(data, dataResults, betas):
-    m=data.shape[0]
-    predicts=hypothesis(data, betas)
-    gradient=np.dot(np.transpose(data), predicts-dataResults)
-    # learningRate is 0.1
-    betas=betas-gradient*( 0.1 / m )
+    m = data.shape[0]
+    predicts = hypothesis(data, betas)
+    gradient = np.dot(np.transpose(data), predicts-dataResults)
+    betas = betas-gradient*( 0.1 / m ) # learningRate is 0.1
     return betas
 
 def iterateGradientDescent(data, dataResults, betas, maxIterationNo, batch):
     cost_history = []
-    dataPartition, dataPartitionResults, batchCount= splitBatches(data, dataResults, batch)
+    dataPartition, results, batchCount= splitBatches(data, dataResults, batch)
     for i in range(maxIterationNo):
         for j in range(batchCount):
-            betas = updateBetas(dataPartition[j], dataPartitionResults[j], betas)
-        cost=calculateCost(data,dataResults,betas)
-        cost_history.append(cost)
-        # treshold is 0.001
-        if len(cost_history)>2 and ((cost_history[-2]-cost_history[-1]) < 0.001):
+            betas = updateBetas(dataPartition[j], results[j], betas)
+        cost_history.append(calculateCost(data,dataResults,betas))
+        if len(cost_history)>2 and ((cost_history[-2]-cost_history[-1]) < 0.001): # treshold is 0.001
             break
     return betas, cost_history
 
